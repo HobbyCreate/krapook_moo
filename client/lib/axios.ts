e9578206b361cff84453462ddf041874cf5a0479
choccolatelatte <63106036+HobbyCreate@users.noreply.github.com>
2026-09-22 05:02:13 +0700

    update api

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "@/store/authStore"; 
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -7,5 +8,18 @@ const api = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+api.interceptors.request.use(
+    (config) => {
+        const token = useAuthStore.getState().token;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
