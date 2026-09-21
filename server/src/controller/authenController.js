b707dbebe052e242999364b0a955a77c52e9e586
choccolatelatte <63106036+HobbyCreate@users.noreply.github.com>
2026-09-22 04:00:43 +0700

    fix sameSite

--- server/src/controller/authenController.js
+++ server/src/controller/authenController.js
@@ -20,7 +20,7 @@ export const login = async (req, res, next) => {
         res.cookie("accessToken", accessToken, {
             httpOnly: true,  
             maxAge: 24 * 60 * 60 * 1000,  
-            sameSite: "strict",
+            sameSite: "none",
             secure: process.env.NODE_ENV === "production"
         });
 
